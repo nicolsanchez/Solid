7aa380ea8875ae9e46709f9084a54970e457e2d8
nicolsanchez <nicolas.sanchez@mercadolibre.com.co>
2021-08-06 15:27:15 -0500

    UPD controller

--- grow spring/src/main/groovy/com/grow/main/controller/ControladorPersona.groovy
+++ grow spring/src/main/groovy/com/grow/main/controller/ControladorPersona.groovy
@@ -5,6 +5,7 @@ import com.grow.main.implementacion.interfaces.IServicioDePersona
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -35,34 +36,17 @@ class ControladorPersona {
         return new ResponseEntity<Persona>(servicioDePersona.obtenerPersonaPorId(id), HttpStatus.OK)
     }
 
-    /*@DeleteMapping(path="/personas/{id}")
+    @DeleteMapping(path="/personas/{id}")
      ResponseEntity<Persona> deleteEmployee(@PathVariable Long id) {
-        ResponseEntity<Persona> respuesta = null
-
-        if(mapaDePersonas.containsKey(id)){
-            mapaDePersonas.remove(id)
-            respuesta = new ResponseEntity(null, HttpStatus.OK)
-        } else {
-            respuesta = new ResponseEntity(null, HttpStatus.NOT_FOUND)
-        }
-        return respuesta
+        return new ResponseEntity<Persona>(servicioDePersona.borrarPersonaPorCedula(id), HttpStatus.OK)
     }
 
-    @GetMapping(path="/personas")
+    /*@GetMapping(path="/personas")
      ResponseEntity<List<Persona>> buscarPersonas(){
         ResponseEntity<List<Persona>> respuesta = null
         respuesta = new ResponseEntity(mapaDePersonas.values(), HttpStatus.OK)
         return respuesta
-    }
-
-    @GetMapping(path="/personas/{id}")
-     ResponseEntity<Persona> obtenerPersona(@PathVariable Long id){
-        ResponseEntity<Persona> respuesta = null
-        if(mapaDePersonas.containsKey(id)) {
-            respuesta = new ResponseEntity(mapaDePersonas.get(id), HttpStatus.OK)
-        } else {
-            respuesta = new ResponseEntity(null, HttpStatus.NOT_FOUND)
-        }
-        return respuesta
     }*/
+
+
 }
